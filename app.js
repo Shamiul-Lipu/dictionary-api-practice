2ffa014dc08a3f0af61834b2f77f9304820b12a8
unknown <shamiullipu@gmail.com>
2023-02-27 12:09:58 +0600

    error fixed and search functionality added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // fetching the data
-const loadData = async () => {
-    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/hello`;
+const loadData = async (searchText) => {
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchText}`;
     const res = await fetch(url);
     const data = await res.json();
     displayData(data[0]);
@@ -11,12 +11,12 @@ const displayData = (wordArray) => {
     console.log(wordArray)
     const { word, sourceUrls, meanings, phonetics } = wordArray;
     console.log(meanings);
-    console.log(meanings[2].definitions);
+    console.log(meanings[2]?.definitions);
     const section = document.getElementById('display-section');
     // display on section
     section.innerHTML = `
     <h3>${word}</h3>
-    <p>${phonetics[1].text}</p>
+    <p>${phonetics[1]?.text ? phonetics[1].text : 'not found'}</p>
     <h5>${meanings[0].partOfSpeech}</h5>
     <div id="noun-meaning-div">
         <h6>Meaning</h6>
@@ -26,10 +26,10 @@ const displayData = (wordArray) => {
         </ul>
     </div>
     <p>Synonyms : ${meanings[0].synonyms}</p>
-    <h5>${meanings[1].partOfSpeech}</h5>
+    <h5>${meanings[1]?.partOfSpeech ? meanings[1].partOfSpeech : 'not found'}</h5>
     <div id="varb-meaning-div">
         <h6>Meaning</h6>
-        <p>${meanings[1].definitions[0].definition}</p>
+        <p>${meanings[1]?.definitions[0].definition ? meanings[1].definitions[0].definition : 'not found'}</p>
         <ul>
 
         </ul>
@@ -40,7 +40,7 @@ const displayData = (wordArray) => {
     const nounDiv = document.getElementById('noun-meaning-div');
 
     const nounUl = document.createElement('ul');
-    meanings[2].definitions.forEach(el => {
+    meanings[2]?.definitions.forEach(el => {
         console.log(el.definition);
         const li = document.createElement('li');
         li.innerText = el.definition;
@@ -51,4 +51,12 @@ const displayData = (wordArray) => {
     console.log(nounUl);
 }
 
+// search
+document.getElementById('search-btn').addEventListener('click', function () {
+    const searchText = document.getElementById('search-input').value;
+    loadData(searchText)
+})
+
+
+// deafult set as undefined because of empty function no value
 loadData();
